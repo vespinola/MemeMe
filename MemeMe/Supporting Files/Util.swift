85c5471e25cc5a8d65e0a91519d8cc3fb5fa988e
Vladimir Espinola <vladimir.espinola@gmail.com>
2017-12-04 09:16:43 -0300

    project was created

---
+++ MemeMe/Supporting Files/Util.swift
@@ -0,0 +1,16 @@
+//
+//  Util.swift
+//  MemeMe
+//
+//  Created by administrator on 12/4/17.
+//  Copyright © 2017 administrator. All rights reserved.
+//
+
+import UIKit
+
+class Util {
+    class func showActivityView(for items: [AnyObject], in viewController: UIViewController) {
+        let controller = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        viewController.present(controller, animated: true, completion: nil)
+    }
+}
